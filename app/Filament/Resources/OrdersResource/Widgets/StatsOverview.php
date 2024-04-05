2f6467f20cd713b69d40494cd04cead3346ade58
FelipeArnold <felipearnold.94@gmail.com>
2024-04-05 16:02:35 -0300

    Commit.

--- app/Filament/Resources/OrdersResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/OrdersResource/Widgets/StatsOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\OrdersResource\Widgets;
 
+use App\Models\Orders;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -11,16 +12,22 @@ class StatsOverview extends BaseWidget
 
     protected function getStats(): array
     {
+        // query orders by period and count orders days
+        $orders = Orders::query()
+            ->selectRaw('count(*) as count, date(created_at) as date, sum(total) as total')
+            ->groupBy('date')
+            ->get();
+
         return [
             stat::make('Unique views', '500.1k')
                 ->description('32k increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart([7, 2, 10])
                 ->color('success'),
-            stat::make('Orders', '1,200')
-                ->description('200 increase')
+            stat::make('Ordens', $orders->pluck('total')->sum())
+                ->description($orders->count() . ' criadas')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->chart([3, 2, 5, 3, 7, 4, 9])
+                ->chart($orders->pluck('count')->toArray())
                 ->color('success'),
             stat::make('Revenue', '$12,000')
                 ->description('2k increase')
