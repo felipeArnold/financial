525892248efc28597e4b03e7003bcf230a525b64
felipeArnold <44820260+felipeArnold@users.noreply.github.com>
2024-04-22 20:58:28 -0300

    Reajustado cadastro de empresa.

--- app/Filament/Pages/Tenancy/RegisterTeam.php
+++ app/Filament/Pages/Tenancy/RegisterTeam.php
@@ -2,10 +2,15 @@
 
 namespace App\Filament\Pages\Tenancy;
 
+use App\Enums\Tenant\PlansEnum;
+use App\Enums\Tenant\TypeTenantEnum;
 use App\Models\Tenant;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
 use Filament\Pages\Tenancy\RegisterTenant;
+use Leandrocfe\FilamentPtbrFormFields\Document;
 
 class RegisterTeam extends RegisterTenant
 {
@@ -19,7 +24,31 @@ class RegisterTeam extends RegisterTenant
         return $form
             ->schema([
                 TextInput::make('name'),
-                // ...
+                Document::make('document')
+                    ->label('CPF/CNPJ')
+                    ->required(),
+                ToggleButtons::make('type')
+                    ->label('Tipo')
+                    ->inline()
+                    ->default(TypeTenantEnum::OTHERS)
+                    ->options([
+                        TypeTenantEnum::VEHICLES->value => TypeTenantEnum::VEHICLES->getLabel(),
+                        TypeTenantEnum::MECHANICS->value => TypeTenantEnum::MECHANICS->getLabel(),
+                        TypeTenantEnum::OTHERS->value => TypeTenantEnum::OTHERS->getLabel(),
+                    ]),
+                ToggleButtons::make('plans')
+                    ->label('Plano')
+                    ->inline()
+                    ->default(PlansEnum::FREE)
+                    ->options([
+                        PlansEnum::TEST->value => PlansEnum::TEST->getLabel(),
+                        PlansEnum::FREE->value => PlansEnum::FREE->getLabel(),
+                        PlansEnum::PREMIUM->value => PlansEnum::PREMIUM->getLabel(),
+                    ]),
+                FileUpload::make('avatar')
+                    ->label('Avatar')
+                    ->acceptedFileTypes(['image/*'])
+                    ->rules(['image', 'max:1024']),
             ]);
     }
 
