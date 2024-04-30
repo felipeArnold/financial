bd453c3aec1b8d53f0dfaed911ebaeda905eba47
Felipe Wust <felipearnold.94@gmail.com>
2024-04-30 19:42:08 -0300

    Adicionado upload de fotos aos produtos.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,9 +7,11 @@ use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Leandrocfe\FilamentPtbrFormFields\Money;
 
 class Product extends Model
@@ -32,58 +34,69 @@ class Product extends Model
         return $this->belongsTo(ProductCategory::class, 'product_category_id');
     }
 
+    public function files(): MorphMany
+    {
+        return $this->morphMany(Files::class, 'fileable');
+    }
+
     public static function getForm(): array
     {
         return [
-            Section::make('Informações do produto')
-                ->description('Preencha as informações do produto')
-                ->schema([
-                    TextInput::make('name')
-                        ->label('Nome')
-                        ->placeholder('Nome do produto')
-                        ->rules([
-                            'required',
-                            'max:255',
-                        ])
-                        ->validationMessages([
-                            'name.required' => 'O campo nome é obrigatório',
-                            'name.max' => 'O campo nome deve ter no máximo 255 caracteres',
-                        ])
-                        ->columnSpan(2),
-                    Select::make('product_category_id')
-                        ->label('Categoria')
-                        ->options(ProductCategory::pluck('name', 'id'))
-                        ->placeholder('Selecione uma categoria')
-                        ->searchable()
-                        ->createOptionForm(function () {
-                            return ProductCategory::getForm();
-                        })
-                        ->createOptionUsing(function (array $data): int {
-                            return ProductCategory::create([
-                                 'tenant_id' => auth()->user()->tenants()->first()->id,
-                                'name' => $data['name']
-                            ])->id;
-                        })
-                        ->native(false)
-                        ->rules([
-                            'required',
-                            'exists:product_categories,id',
-                        ])
-                        ->validationMessages([
-                            'category.required' => 'O campo categoria é obrigatório',
-                            'category.exists' => 'A categoria selecionada não existe',
-                        ]),
-                    TextInput::make('stock')
-                        ->label('Estoque')
-                        ->numeric(),
-                    PtbrMoney::make('cost_price')
-                        ->label('Preço de custo'),
-                    PtbrMoney::make('sale_price')
-                        ->label('Preço de venda'),
-                    MarkdownEditor::make('description')
-                        ->label('Descrição')
-                        ->columnSpan(2)
-                ])->columns(2),
+            Wizard::make([
+                Wizard\Step::make('Dados gerais')
+                    ->icon('heroicon-o-information-circle')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nome')
+                            ->placeholder('Nome do produto')
+                            ->rules([
+                                'required',
+                                'max:255',
+                            ])
+                            ->validationMessages([
+                                'name.required' => 'O campo nome é obrigatório',
+                                'name.max' => 'O campo nome deve ter no máximo 255 caracteres',
+                            ])
+                            ->columnSpan(2),
+                        Select::make('product_category_id')
+                            ->label('Categoria')
+                            ->options(ProductCategory::pluck('name', 'id'))
+                            ->placeholder('Selecione uma categoria')
+                            ->searchable()
+                            ->createOptionForm(function () {
+                                return ProductCategory::getForm();
+                            })
+                            ->createOptionUsing(function (array $data): int {
+                                return ProductCategory::create([
+                                     'tenant_id' => auth()->user()->tenants()->first()->id,
+                                    'name' => $data['name']
+                                ])->id;
+                            })
+                            ->native(false)
+                            ->rules([
+                                'required',
+                                'exists:product_categories,id',
+                            ])
+                            ->validationMessages([
+                                'category.required' => 'O campo categoria é obrigatório',
+                                'category.exists' => 'A categoria selecionada não existe',
+                            ]),
+                        TextInput::make('stock')
+                            ->label('Estoque')
+                            ->numeric(),
+                        PtbrMoney::make('cost_price')
+                            ->label('Preço de custo'),
+                        PtbrMoney::make('sale_price')
+                            ->label('Preço de venda'),
+                        MarkdownEditor::make('description')
+                            ->label('Descrição')
+                            ->columnSpan(2),
+                    ]),
+                Wizard\Step::make('Arquivos')
+                    ->schema(Files::getForm()),
+            ])
+                ->skippable()
+                ->columnSpan(2)
         ];
     }
 }
